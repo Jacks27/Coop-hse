d64bc85c70bc0f3236c6dfb68f1210e0c3773da7
Jacks27 <simon.jackson.kariuki@gmail.com>
2019-05-23 07:35:21 +0300

    edit home

--- tests/v1/test_auth.py
+++ tests/v1/test_auth.py
@@ -1,8 +1,11 @@
 import json
 from tests.v1 import BaseTest
 
-class TestAuth(BaseTest):
 
+class TestAuth(BaseTest):
+    user_email={
+        "email": "lilu@quickmail.rocks"
+        }
     def test_signup(self):
         data = self.generate_random_user()
         result = self.auth_request('/app/v1/signup', 'POST', data)
@@ -11,6 +14,13 @@ class TestAuth(BaseTest):
         datacheck = json.loads(result.data)
         self.check_standard_reply(datacheck, 201)
 
+    def test_signup_wrongdata(self):
+        data = self.generate_random_user()
+        data['email']= "jacks.com"
+        result = self.auth_request('/app/v1/signup', 'POST', data)
+        print(json.loads(result.data))
+        self.assertEqual(result.status_code, 403)
+        
     def test_login(self):
         data=self.login()
         result= self.auth_request('app/v1/login', 'POST', data)
@@ -26,34 +36,65 @@ class TestAuth(BaseTest):
         datacheck2 = json.loads(result.data)
         self.check_standard_reply(datacheck2, 400, True)
     def test_make_admin(self):
-        data=   {
-        "email": "lilu@quickmail.rocks"
-        }
-        result = self.auth_request('app/v1/create_admin', 'PATCH', data)
+        result = self.auth_request('app/v1/create_admin', 'PATCH', data=self.user_email)
         self.assertEqual(result.status_code, 201)
         datacheck = json.loads(result.data)
-
         self.check_standard_reply(datacheck, 201)
+    def test_getuser(self):
+        result=self.auth_request('app/v1/get_users', 'GET', data=None)
+        self.assertEqual(result.status_code, 200)
+        datacheck = json.loads(result.data)
+        self.check_standard_reply(datacheck, 200)
+    def get_singeUser(self):
+        result=self.auth_request('app/v1/Users/1', 'GET', data=None)
+        self.assertEqual(result.status_code, 200)
+        datacheck = json.loads(result.data)
+        self.check_standard_reply(datacheck, 200)
+    def get_wrng_user(self):
+        result=self.auth_request('app/v1/Users/4', 'GET', data=None)
+        self.assertEqual(result.status_code, 404)
+        datacheck = json.loads(result.data)
+        self.check_standard_reply(datacheck,404, True)
+
+    def test_wrongdetails(self):
+        self.user_email['email'] = 'llul@gamil.com'
+        result = self.auth_request('app/v1/create_admin', 'PATCH', data=self.user_email)
+        self.assertEqual(result.status_code, 404)
+        datacheck=json.loads(result.data)
+        self.check_standard_reply(datacheck, 404, True)
+
 
     def test_forgot_password(self):
-        data = {
-            "email": "lilu@quickdmail.rocks"
-            }
-        result = self.auth_request('app/v1/forgot_password', 'POST', data)
-        self.assertEqual(result.status_code, 400)
-        datacheck = json.loads(result.data)
-        self.check_standard_reply(datacheck, 400, True)
+        Userdata={"email":"lyys@gmail.com"}
+        result=self.auth_request('app/v1/forgot_password', 'POST', Userdata)
+        self.assertEqual(result.status_code, 404)
+
+    def test_send_emailforgot_password(self):
+        Email={"email":"lilu@quickmail.rocks"}
+        result=self.auth_request('app/v1/forgot_password', 'POST', Email)
+        self.assertEqual(result.status_code, )
 
     def test_recover_password(self):
-        data = {
+        passwords ={
         "password":"1234567890",
         "confirmpassword":"1234567890"
         }
-        result = self.auth_request('app/v1/recover_account/Imh1YnVodXR1c2FAcm95YWxtYXJrZXQub25saW5lIg.XMouRw.81GP9i44qBDxdZr8TzInSLSVe5w/hubuhutusa%40royalmarket.online', 'POST', data)
+        result = self.auth_request('app/v1/recover_account/Imh1YnVodXR1c2FAcm95YWxtYXJrZXQub25saW5lIg.XMouRw.81GP9i44qBDxdZr8TzInSLSVe5w/hubuhutusa%40royalmarket.online', 'POST', passwords)
         self.assertEqual(result.status_code, 403)
         datacheck = json.loads(result.data)
         self.check_standard_reply(datacheck, 403, True)
-        
-    def
+
+    def test_confirmEmail(self):
+        data=None
+        result = self.auth_request('app/v1/confirm_email/Imh1YnVodXR1c2FAcm95YWxtYXJrZXQub25saW5lIg.XMouRw.81GP9i44qBDxdZr8TzInSLSVe5w/hubuhutusa%40royalmarket.online', 'POST', data)
+        self.assertEqual(result.status_code,400)
+        datacheck = json.loads(result.data)
+        self.check_standard_reply(datacheck, 400, True)
+    def test_update_users(self):
+        data =self.generate_random_user()
+        data.update({"id": 1})
+        data['email']= 'lilu@quickmail.rocks'
+        result = self.auth_request('app/v1/user_info_update', 'PATCH', data)
+        self.assertEqual(result.status_code, 403)
 
 
